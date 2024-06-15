0bc6b2492dc047116a44b6f3a8fb1fe43fbe97a6
AutisticLulu <discord.meguminbot@gmail.com>
2024-06-15 21:37:31 +0200

    Improved: Toggle Zoom on Beat.lua
    * Implemented an optional variable to "Zoom every number of beats" instead of always on every beat.
    * Parse function is now local.
    * Simplified code logic.
    * Added list of Psych Engine versions that are tested and confirmed working as intended.

--- example_mods/custom_events/Toggle Zoom on Beat.lua
+++ example_mods/custom_events/Toggle Zoom on Beat.lua
@@ -1,40 +1,43 @@
 --[[
 >>> Toggleable Add Camera Zoom Event for Psych Engine. // AutisticLulu
->>> Triggers Add Camera Zoom events on every beat.
+	Triggers Add Camera Zoom events on every beat or optionally between a set amount of beats.
+	
+	Confirmed compatible Psych Engine Versions:
+	0.6.x, 0.7.1, 0.7.2
 
 Value 1:
-	Parameters: "Stage Zoom, Hud Zoom"
-	(Example: 0.015, 0.03)
+    Parameters: "Stage Zoom, Hud Zoom, Number of beats *OPTIONAL*"
+    (Example: 0.015, 0.03)
+    (Example: 0.015, 0.03, 2)
 
 Value 2:
-	Parameters: [On/Off]
-	(Example: On)
+    Parameters: [On/Off]
+    (Example: On)
 ]]
 
 -- Define local variables.
 local isActive = false
-local zoomValues = nil
+local gameZoom, hudZoom, numBeats = nil, nil, nil
+local beatTrigger = 1
+local beatCount = 0
 
--- This function is called when an event occurs.
+-- Helper function: Parses the gameZoom, hudZoom, and numBeats.
+local function parseZoomValues(value)
+    return value:match('([^,]+),%s*([^,]+),?%s*([^,]*)')
+end
+
 function onEvent(name, value1, value2)
-	if name == 'Toggle Zoom on Beat' or name == 'Toggle_Zoom_on_Beat' and cameraZoomOnBeat then
-		zoomValues = value1
-		if value2 == 'On' then
-			isActive = true
-		elseif value2 == 'Off' then
-			isActive = false
-		end
-	end
+    if (name == 'Toggle Zoom on Beat' or name == 'Toggle_Zoom_on_Beat') and cameraZoomOnBeat then
+        gameZoom, hudZoom, numBeats = parseZoomValues(value1)
+        
+        beatTrigger = tonumber(numBeats) or 1 -- Default to 1 if not specified or invalid
+        isActive = (value2 == 'On')
+    end
 end
 
 function onBeatHit()
-	if isActive then
-		local gameZoom, hudZoom = parseZoomValues(zoomValues)
-		triggerEvent("Add Camera Zoom", gameZoom, hudZoom)
-	end
-end
-
--- Helper function: Parses the gameZoom and hudZoom values from the input string.
-function parseZoomValues(value)
-	return value:gsub(' ', ''):match('([^,]*),([^,]*)')
+    beatCount = beatCount + 1
+    if isActive and beatCount % beatTrigger == 0 and gameZoom and hudZoom then
+        triggerEvent("Add Camera Zoom", gameZoom, hudZoom)
+    end
 end
